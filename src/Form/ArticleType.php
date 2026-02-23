3c0203d4a6d5f79dcb943b4ada493aef29c573f0
Mehdi dahmouni <mehdidahmouni24@gmail.com>
2026-02-23 16:58:19 +0100

    feat: Entité Categorie, relation ManyToOne, CRUD catégories et EntityType

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -11,6 +11,11 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Categorie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+
+
 
 class ArticleType extends AbstractType
 {
@@ -55,6 +60,14 @@ class ArticleType extends AbstractType
             ->add('enregistrer', SubmitType::class, [
                 'label' => '💾 Enregistrer',
                 'attr' => ['class' => 'btn btn-primary w-100'],
+            ])
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'nom',
+                'label' => 'Catégorie',
+                'placeholder' => 'Sélectionnez une catégorie',
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
             ])
         ;
     }
